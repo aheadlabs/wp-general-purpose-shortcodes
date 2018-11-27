8a6ca80c8259ea72866f0b80298984d250fcccc3
Iván Sainz (SXO) <ivan.sainz@aheadlabs.com>
2018-11-27 16:20:23 +0100

    Added shortcode for calculating the number of years from a given date until now.

--- wp-general-purpose-shortcodes/wp-general-purpose-shortcodes.php
+++ wp-general-purpose-shortcodes/wp-general-purpose-shortcodes.php
@@ -3,21 +3,39 @@
 Plugin Name: WP general purpose shortcodes
 Plugin URI:   https://developer.wordpress.org/plugins/wp-general-purpose-shortcodes/
 Description:  General purpose shortcodes for WordPress
-Version:      0.0.1
+Version:      0.2.0
 Author:       Iván Sainz (Ahead Labs)
 Author URI:   https://ivansainz.es/
 License:      GPL-2.0
 License URI:  https://github.com/ivansainz/wp-general-purpose-shortcodes/blob/master/LICENSE
 */
 
+/**
+ * Shortcode functions
+ */
 function ahlCurrentYear() {
     return date('Y');
 }
 
-function ahlShortcodesInit(){
-    add_shortcode('ahl_year', 'ahlCurrentYear');
+function ahlYearsFromDate($atts) {
+    $date1 = date_create($atts["date"]);
+    $date2 = date_create();
+
+    $interval = date_diff($date2, $date1);
+    return $interval->format('%y');
 }
 
+/**
+ * Wiring everything up...
+ */
+
+// adds the shortcode functions one by one
+function ahlShortcodesInit(){
+    add_shortcode('ahl_year', 'ahlCurrentYear');
+    add_shortcode('ahl_yearsFromDate', 'ahlYearsFromDate');
+}
+
+// adds all shortcodes
 add_action('init', 'ahlShortcodesInit');
 
 ?>
